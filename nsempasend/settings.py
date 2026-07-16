df65e532490e8492cc519e3573f05e8cefa1b34b
Godwin Boakye <ohenebayb@gmail.com>
2026-07-16 17:54:56 +0000

    email smtp setup updated

--- nsempasend/settings.py
+++ nsempasend/settings.py
@@ -147,7 +147,7 @@ EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
-EMAIL_HOST_USER = 'no-reply@nsempaservice.com'
-EMAIL_HOST_PASSWORD = ',TLZ(4j$PkyP'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = 'NsempaSend <no-reply@nsempaservice.com>'
